f58370e1c805d24e072aedb29b0cf80f73a0126c
Elejota <93092515+Elejota98@users.noreply.github.com>
2023-10-12 08:30:57 -0500

    Version actual bucarica

---
+++ Liquidador/PARKING/MC.ModuloEntrada.WinForm.FrontEnd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MC.ModuloEntrada.WinForm.FrontEnd
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (System.Diagnostics.Process.GetProcessesByName("MC.ModuloEntrada.WinForm.FrontEnd").Length > 1)
+            {
+                MessageBox.Show("Ya existe una Instancia en Ejecución..");
+                return;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmPrincipal());
+        }
+    }
+}
